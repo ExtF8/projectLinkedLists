393c8481ddffa9d9b9f3b64f7ba3b84406b93fbd
extf8 <extfield8@gmail.com>
2024-04-22 13:59:08 +0300

    feat: add ListNode class constructor and rename file

---
+++ ListNode.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a node in a linked list.
+ */
+export class ListNode {
+    /**
+     * Creates a new instance of ListNode.
+     * @param {*} value - The value to be stored in the node.
+     */
+    constructor(value) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
